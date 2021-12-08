b94a86929cdcfb943b334a4f60149c693c0e59a9
Jeevan Ashok Sawalkar <54221345+JeevanSawalkar@users.noreply.github.com>
2021-12-08 12:56:56 +0530

    Update bmiCalculator.js

--- 4. JavaScript/bmiCalculator.js
+++ 4. JavaScript/bmiCalculator.js
@@ -1,4 +1,4 @@
-//Create your function below this line
+//simple solution using fn and formula 
 
 function bmiCalculator(weight, height) {
   var myBmi = weight / (Math.pow(height,2));
@@ -8,10 +8,21 @@ function bmiCalculator(weight, height) {
 var bmi = bmiCalculator(75, 1.87);
 console.log(bmi);
 
-/* If my weight is 65kg and my height is 1.8m, I should be able to call your function like this:
 
-var bmi = bmiCalculator(65, 1.8);
+// same problem using comparators 
+function bmiCalculator (weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+    var interpretation = "";  // FYI declaring string variable 
 
-bmi should equal around 20 in this case.
-
-*/
+   
+    if(bmi<18.5){
+        interpretation = "Your BMI is " + bmi + ", " + "so you are underweight.";
+    } else if (bmi>=18.5 && bmi<24.9) {
+        interpretation = "Your BMI is " + bmi + ", " + "so you have a normal weight.";
+    } else if (bmi>=24.9) {
+        interpretation = "Your BMI is " + bmi + ", " + "so you are overweight.";
+    }  
+    return interpretation;
+}
+console.log(bmiCalculator(45,6));
+// the above fn call is typed by me and it is correct . FYI 
